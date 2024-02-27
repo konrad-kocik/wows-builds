113da5e3b3d9fa159234cf2be92d3444888edba7
Konrad Kocik <konrad.kocik@gmail.com>
2024-02-27 22:39:05 +0100

    Add all battleship skills to model

--- model/skills.py
+++ model/skills.py
@@ -13,6 +13,54 @@ class Skill:
 
 
 gun_feeder = Skill('Gun Feeder', 1)
+demolition_expert = Skill('Demolition Expert', 1)
+consumables_specialist = Skill('Consumables Specialist', 1)
 emergency_repair_specialist = Skill('Emergency Repair Specialist', 1)
+incoming_fire_alert = Skill('Incoming Fire Alert', 1)
+preventive_maintenance = Skill('Preventive Maintenance', 1)
+
 grease_the_gears = Skill('Grease the Gears', 2)
+inertia_fuse_for_he_shells = Skill('Inertia Fuse for HE Shells', 2)
+brisk = Skill('Brisk', 2)
+vigilance = Skill('Vigilance', 2)
+priority_target = Skill('Priority Target', 2)
+aa_defense_and_asw_expert = Skill('AA Defense and ASW Expert', 2)
+
+super_heavy_ap_shells = Skill('Super-Heavy AP Shells', 3)
+long_range_secondary_battery_shells = Skill('Long-Range Secondary Battery Shells', 3)
 adrenaline_rush = Skill('Adrenaline Rush', 3)
+basics_of_survivability = Skill('Basics of Survivability', 3)
+improved_repair_party_readiness = Skill('Improved Repair Party Readiness', 3)
+focus_fire_training = Skill('Focus Fire Training', 3)
+
+furious = Skill('Furious', 4)
+manual_secondary_battery_aiming = Skill('Manual Secondary Battery Aiming', 4)
+close_quarters_combat = Skill('Close Quarters Combat', 4)
+emergency_repair_expert = Skill('Emergency Repair Expert', 4)
+concealment_expert = Skill('Concealment Expert', 4)
+fire_prevention_expert = Skill('Fire Prevention Expert', 4)
+
+BATTLESHIP_SKILLS = [gun_feeder,
+                     demolition_expert,
+                     consumables_specialist,
+                     emergency_repair_specialist,
+                     incoming_fire_alert,
+                     preventive_maintenance,
+                     grease_the_gears,
+                     inertia_fuse_for_he_shells,
+                     brisk,
+                     vigilance,
+                     priority_target,
+                     aa_defense_and_asw_expert,
+                     super_heavy_ap_shells,
+                     long_range_secondary_battery_shells,
+                     adrenaline_rush,
+                     basics_of_survivability,
+                     improved_repair_party_readiness,
+                     focus_fire_training,
+                     furious,
+                     manual_secondary_battery_aiming,
+                     close_quarters_combat,
+                     emergency_repair_expert,
+                     concealment_expert,
+                     fire_prevention_expert]
--- tests/test_skills.py
+++ tests/test_skills.py
@@ -1,10 +1,31 @@
 from pytest import fixture
 
 from model.skills import Skill
+from model.skills import BATTLESHIP_SKILLS
 from model.skills import (gun_feeder,
+                          demolition_expert,
+                          consumables_specialist,
                           emergency_repair_specialist,
+                          incoming_fire_alert,
+                          preventive_maintenance,
                           grease_the_gears,
-                          adrenaline_rush)
+                          inertia_fuse_for_he_shells,
+                          brisk,
+                          vigilance,
+                          priority_target,
+                          aa_defense_and_asw_expert,
+                          super_heavy_ap_shells,
+                          long_range_secondary_battery_shells,
+                          adrenaline_rush,
+                          basics_of_survivability,
+                          improved_repair_party_readiness,
+                          focus_fire_training,
+                          furious,
+                          manual_secondary_battery_aiming,
+                          close_quarters_combat,
+                          emergency_repair_expert,
+                          concealment_expert,
+                          fire_prevention_expert)
 
 
 @fixture
@@ -30,9 +51,29 @@ def test_gun_feeder_has_correct_attributes():
     assert gun_feeder._cost == 1
 
 
+def test_demolition_expert_has_correct_attributes():
+    assert demolition_expert._name == 'Demolition Expert'
+    assert demolition_expert._cost == 1
+
+
+def test_consumables_specialist_has_correct_attributes():
+    assert consumables_specialist._name == 'Consumables Specialist'
+    assert consumables_specialist._cost == 1
+
+
 def test_emergency_repair_specialist_has_correct_attributes():
     assert emergency_repair_specialist._name == 'Emergency Repair Specialist'
     assert emergency_repair_specialist._cost == 1
+
+
+def test_incoming_fire_alert_has_correct_attributes():
+    assert incoming_fire_alert._name == 'Incoming Fire Alert'
+    assert incoming_fire_alert._cost == 1
+
+
+def test_preventive_maintenance_has_correct_attributes():
+    assert preventive_maintenance._name == 'Preventive Maintenance'
+    assert preventive_maintenance._cost == 1
 
 
 def test_grease_the_gears_has_correct_attributes():
@@ -40,6 +81,113 @@ def test_grease_the_gears_has_correct_attributes():
     assert grease_the_gears._cost == 2
 
 
+def test_inertia_fuse_for_he_shells_has_correct_attributes():
+    assert inertia_fuse_for_he_shells._name == 'Inertia Fuse for HE Shells'
+    assert inertia_fuse_for_he_shells._cost == 2
+
+
+def test_brisk_has_correct_attributes():
+    assert brisk._name == 'Brisk'
+    assert brisk._cost == 2
+
+
+def test_vigilance_has_correct_attributes():
+    assert vigilance._name == 'Vigilance'
+    assert vigilance._cost == 2
+
+
+def test_priority_target_has_correct_attributes():
+    assert priority_target._name == 'Priority Target'
+    assert priority_target._cost == 2
+
+
+def test_aa_defense_and_asw_expert_has_correct_attributes():
+    assert aa_defense_and_asw_expert._name == 'AA Defense and ASW Expert'
+    assert aa_defense_and_asw_expert._cost == 2
+
+
+def test_super_heavy_ap_shells_has_correct_attributes():
+    assert super_heavy_ap_shells._name == 'Super-Heavy AP Shells'
+    assert super_heavy_ap_shells._cost == 3
+
+
+def test_long_range_secondary_battery_shells_has_correct_attributes():
+    assert long_range_secondary_battery_shells._name == 'Long-Range Secondary Battery Shells'
+    assert long_range_secondary_battery_shells._cost == 3
+
+
 def test_adrenaline_rush_has_correct_attributes():
     assert adrenaline_rush._name == 'Adrenaline Rush'
     assert adrenaline_rush._cost == 3
+
+
+def test_basics_of_survivability_has_correct_attributes():
+    assert basics_of_survivability._name == 'Basics of Survivability'
+    assert basics_of_survivability._cost == 3
+
+
+def test_improved_repair_party_readiness_has_correct_attributes():
+    assert improved_repair_party_readiness._name == 'Improved Repair Party Readiness'
+    assert improved_repair_party_readiness._cost == 3
+
+
+def test_focus_fire_training_has_correct_attributes():
+    assert focus_fire_training._name == 'Focus Fire Training'
+    assert focus_fire_training._cost == 3
+
+
+def test_furious_has_correct_attributes():
+    assert furious._name == 'Furious'
+    assert furious._cost == 4
+
+
+def test_manual_secondary_battery_aiming_has_correct_attributes():
+    assert manual_secondary_battery_aiming._name == 'Manual Secondary Battery Aiming'
+    assert manual_secondary_battery_aiming._cost == 4
+
+
+def test_close_quarters_combat_has_correct_attributes():
+    assert close_quarters_combat._name == 'Close Quarters Combat'
+    assert close_quarters_combat._cost == 4
+
+
+def test_emergency_repair_expert_has_correct_attributes():
+    assert emergency_repair_expert._name == 'Emergency Repair Expert'
+    assert emergency_repair_expert._cost == 4
+
+
+def test_concealment_expert_has_correct_attributes():
+    assert concealment_expert._name == 'Concealment Expert'
+    assert concealment_expert._cost == 4
+
+
+def test_fire_prevention_expert_has_correct_attributes():
+    assert fire_prevention_expert._name == 'Fire Prevention Expert'
+    assert fire_prevention_expert._cost == 4
+
+
+def test_battleship_skills_are_correct():
+    assert BATTLESHIP_SKILLS == [gun_feeder,
+                                 demolition_expert,
+                                 consumables_specialist,
+                                 emergency_repair_specialist,
+                                 incoming_fire_alert,
+                                 preventive_maintenance,
+                                 grease_the_gears,
+                                 inertia_fuse_for_he_shells,
+                                 brisk,
+                                 vigilance,
+                                 priority_target,
+                                 aa_defense_and_asw_expert,
+                                 super_heavy_ap_shells,
+                                 long_range_secondary_battery_shells,
+                                 adrenaline_rush,
+                                 basics_of_survivability,
+                                 improved_repair_party_readiness,
+                                 focus_fire_training,
+                                 furious,
+                                 manual_secondary_battery_aiming,
+                                 close_quarters_combat,
+                                 emergency_repair_expert,
+                                 concealment_expert,
+                                 fire_prevention_expert]
