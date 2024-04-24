0309619e13a04d28df03ec8bbefad89faaca97fc
Konrad Kocik <konrad.kocik@gmail.com>
2024-04-24 21:04:51 +0200

    Add unit tests for ListBuilds

--- tests/unit_tests/test_cli/test_states.py
+++ tests/unit_tests/test_cli/test_states.py
@@ -1,7 +1,7 @@
 from pytest import fixture
 from unittest.mock import Mock, patch, call
 
-from cli.states import Transition, State, Start, Exit, ListBuilds, EditBuild
+from cli.states import Transition, State, Start, Exit, ListBuilds, ShowBuild, EditBuild
 
 
 @fixture
@@ -24,6 +24,11 @@ def exit():
     return Exit()
 
 
+@fixture
+def list_builds():
+    return ListBuilds()
+
+
 def test_state_has_correct_attributes(state):
     assert state._builds == []
     assert state._header == ''
@@ -42,7 +47,7 @@ def test_state_execute_does_correct_execution(state):
 
     state.execute()
 
-    state._execute.assert_called_once()
+    state._execute.assert_called_once_with()
 
 
 def test_go_to_navigates_through_correct_transition(state):
@@ -72,7 +77,7 @@ def test_state__execute_does_correct_execution(state):
         state._execute()
 
     mocked_print.assert_called_once_with(f'{state._header}\n')
-    state._show_transitions.assert_called_once()
+    state._show_transitions.assert_called_once_with()
 
 
 def test_state__show_transitions_prints_correct_transitions(state):
@@ -170,8 +175,8 @@ def test_start_when_no_builds_and_they_can_be_loaded_then_they_are_loaded(builds
     with patch('cli.states.can_builds_be_loaded', can_builds_be_loaded), patch('cli.states.load_builds', load_builds):
         start = Start()
 
-    can_builds_be_loaded.assert_called_once()
-    load_builds.assert_called_once()
+    can_builds_be_loaded.assert_called_once_with()
+    load_builds.assert_called_once_with()
     assert start._builds == builds
 
 
@@ -183,7 +188,7 @@ def test_start_when_no_builds_and_they_cannot_be_loaded_then_they_are_not_loaded
     with patch('cli.states.can_builds_be_loaded', can_builds_be_loaded), patch('cli.states.load_builds', load_builds):
         start = Start()
 
-    can_builds_be_loaded.assert_called_once()
+    can_builds_be_loaded.assert_called_once_with()
     load_builds.assert_not_called()
     assert start._builds == []
 
@@ -219,3 +224,109 @@ def test_exit__execute_does_correct_execution(exit):
 
     mocked_print.assert_called_once_with(f'{exit._header}\n')
     mocked_exit.assert_called_once_with()
+
+
+def test_list_builds_has_correct_attributes(list_builds):
+    assert list_builds._header == 'BUILDS'
+    assert list_builds._transitions == []
+
+
+def test_list_builds__execute_does_correct_execution(list_builds):
+    list_builds._show_grouped_builds = Mock()
+    mocked_print = Mock()
+
+    with patch('builtins.print', mocked_print):
+        list_builds._execute()
+
+    mocked_print.assert_has_calls([call(f'{list_builds._header}'),
+                                   call(f'\n[0] Back\n')])
+    list_builds._show_grouped_builds.assert_called_once_with()
+    assert Transition(0, 'Back', list_builds._start) in list_builds._transitions
+
+
+def test_list_builds__show_grouped_builds_prints_builds_in_correct_order(list_builds):
+    build_battleship_1 = Mock()
+    build_battleship_1.ship.ship_class = 'Battleship'
+    build_battleship_1.name = 'build_battleship_1'
+    build_battleship_2 = Mock()
+    build_battleship_2.ship.ship_class = 'Battleship'
+    build_battleship_2.name = 'build_battleship_2'
+    build_cruiser_1 = Mock()
+    build_cruiser_1.ship.ship_class = 'Cruiser'
+    build_cruiser_1.name = 'build_cruiser_1'
+    build_cruiser_2 = Mock()
+    build_cruiser_2.ship.ship_class = 'Cruiser'
+    build_cruiser_2.name = 'build_cruiser_2'
+    list_builds._builds = [build_cruiser_1, build_battleship_1, build_cruiser_2, build_battleship_2]
+    mocked_print = Mock()
+
+    with patch('builtins.print', mocked_print):
+        list_builds._show_grouped_builds()
+
+    mocked_print.assert_has_calls([call(f'\nBattleships:'),
+                                   call(f'  [1] {build_battleship_1.name}'),
+                                   call(f'  [2] {build_battleship_2.name}'),
+                                   call(f'\nCruisers:'),
+                                   call(f'  [3] {build_cruiser_1.name}'),
+                                   call(f'  [4] {build_cruiser_2.name}')])
+    assert list_builds._transitions == [Transition(1, build_battleship_1.name, list_builds._show_build),
+                                        Transition(2, build_battleship_2.name, list_builds._show_build),
+                                        Transition(3, build_cruiser_1.name, list_builds._show_build),
+                                        Transition(4, build_cruiser_2.name, list_builds._show_build)]
+
+
+def test_list_builds__go_to_when_transition_is_found_and_its_id_is_not_a_zero_then_build_is_used_and_next_state_is_returned(list_builds):
+    build = Mock()
+    State._builds = [build]
+    next_state = State()
+    transition_id = 1
+    list_builds._transitions = [Transition(0, 'Transition 0', Mock(return_value=None)),
+                                Transition(1, 'Transition 1', Mock(return_value=next_state))]
+
+    returned_state = list_builds._go_to(transition_id=transition_id)
+
+    list_builds._transitions[transition_id].next_state.assert_called_once_with(build=build)
+    assert returned_state == next_state
+
+
+def test_list_builds__go_to_when_transition_is_found_and_its_id_is_a_zero_then_build_is_not_used_and_next_state_is_returned(list_builds):
+    next_state = State()
+    transition_id = 0
+    list_builds._transitions = [Transition(0, 'Transition 0', Mock(return_value=next_state)),
+                                Transition(1, 'Transition 1', Mock(return_value=None))]
+
+    returned_state = list_builds._go_to(transition_id=transition_id)
+
+    list_builds._transitions[transition_id].next_state.assert_called_once_with()
+    assert returned_state == next_state
+
+
+def test_list_builds__go_to_when_transition_is_not_found_then_state_returns_itself(list_builds):
+    list_builds._transitions = [Transition(0, 'Transition 0', Mock()),
+                                Transition(1, 'Transition 1', Mock())]
+
+    returned_state = list_builds._go_to(transition_id=3)
+
+    assert returned_state is list_builds
+
+
+def test_list_builds__start_returns_correct_state(list_builds):
+    returned_state = list_builds._start()
+
+    assert isinstance(returned_state, Start)
+
+
+def test_list_builds__show_build_returns_correct_state(list_builds):
+    returned_state = list_builds._show_build(build=Mock())
+
+    assert isinstance(returned_state, ShowBuild)
+
+
+def test_list_builds__show_build_uses_correct_build(list_builds):
+    build = Mock()
+    show_build_state = Mock()
+
+    with patch('cli.states.ShowBuild', show_build_state):
+        list_builds._show_build(build=build)
+
+    show_build_state.assert_called_once_with(build)
