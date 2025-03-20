33d3c3d068d40266d76da6bba55b99937828baa5
Konrad Kocik <konrad.kocik@gmail.com>
2025-03-20 19:40:16 +0100

    Fix ship factory

--- model/ships/ship_factory.py
+++ model/ships/ship_factory.py
@@ -1,20 +1,31 @@
 from model.ships.ship import Ship
+
 from model.ships.amagi import Amagi
+from model.ships.blyskawica import Blyskawica
+from model.ships.devonshire import Devonshire
 from model.ships.gneisenau import Gneisenau
+from model.ships.hawkins import Hawkins
 from model.ships.helena import Helena
 from model.ships.hipper import Hipper
+from model.ships.iron_duke import IronDuke
 from model.ships.minsk import Minsk
 from model.ships.nagato import Nagato
 from model.ships.north_carolina import NorthCarolina
+from model.ships.queen_elizabeth import QueenElizabeth
 from model.ships.vauquelin import Vauquelin
 
 ships = {'Amagi': Amagi,
+         'Błyskawica': Blyskawica,
+         'Devonshire': Devonshire,
          'Gneisenau': Gneisenau,
+         'Hawkins': Hawkins,
          'Helena': Helena,
          'Hipper': Hipper,
+         'Iron Duke': IronDuke,
          'Minsk': Minsk,
          'Nagato': Nagato,
          'North Carolina': NorthCarolina,
+         'Queen Elizabeth': QueenElizabeth,
          'Vauquelin': Vauquelin}
 
 
--- tests/unit_tests/test_model/test_ships/test_ship_factory.py
+++ tests/unit_tests/test_model/test_ships/test_ship_factory.py
@@ -1,24 +1,35 @@
 from pytest import mark
 
 from model.ships.ship_factory import create_ship
+
 from model.ships.amagi import Amagi
+from model.ships.blyskawica import Blyskawica
+from model.ships.devonshire import Devonshire
 from model.ships.gneisenau import Gneisenau
+from model.ships.hawkins import Hawkins
 from model.ships.helena import Helena
 from model.ships.hipper import Hipper
+from model.ships.iron_duke import IronDuke
 from model.ships.minsk import Minsk
 from model.ships.nagato import Nagato
 from model.ships.north_carolina import NorthCarolina
+from model.ships.queen_elizabeth import QueenElizabeth
 from model.ships.vauquelin import Vauquelin
 
 
 @mark.parametrize('ship_name, ship_type',
                   [('Amagi', Amagi),
+                   ('Błyskawica', Blyskawica),
+                   ('Devonshire', Devonshire),
                    ('Gneisenau', Gneisenau),
+                   ('Hawkins', Hawkins),
                    ('Helena', Helena),
                    ('Hipper', Hipper),
+                   ('Iron Duke', IronDuke),
                    ('Minsk', Minsk),
                    ('Nagato', Nagato),
                    ('North Carolina', NorthCarolina),
+                   ('Queen Elizabeth', QueenElizabeth),
                    ('Vauquelin', Vauquelin)])
 def test_create_ship_returns_correct_ship(ship_name, ship_type):
     ship = create_ship(name=ship_name)
